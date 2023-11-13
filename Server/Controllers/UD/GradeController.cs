975e867699b2aedacd9d19d2119e4cf5f0a418aa
sam <sjglover@udel.edu>
2023-11-13 16:28:05 -0500

    Finished GradeController.cs

---
+++ Server/Controllers/UD/GradeController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+using System.Diagnostics;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GradeController : BaseController, GenericRestController<GradeDTO>
+    {
+        public GradeController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        // fix this one 
+        [HttpGet]
+        [Route("Get/{SchoolID}/{SectionID}/{StudentID}/{GradeTypeCode}/{GradeCodeOccurrence}")]
+        public async Task<IActionResult> Get(int SchoolID, int StudentID, int SectionID, string GradeTypeCode, int GradeCodeOccurrence)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeDTO? result = await _context
+                    .Grades
+                    .Where(x => x.SchoolId == SchoolID)
+                    .Where(x => x.SectionId == SectionID)
+                    .Where(x => x.StudentId == StudentID)
+                    .Where(x => x.GradeTypeCode == GradeTypeCode)
+                    .Where(x => x.GradeCodeOccurrence == GradeCodeOccurrence)
+                     .Select(sp => new GradeDTO
+                     {
+                         GradeCodeOccurrence = sp.GradeCodeOccurrence,
+                         GradeTypeCode = sp.GradeTypeCode,
+                         StudentId = sp.StudentId,
+                         SectionId = sp.SectionId,
+                         Comments = sp.Comments,
+                         CreatedBy = sp.CreatedBy,
+                         CreatedDate = sp.CreatedDate,
+                         ModifiedBy = sp.ModifiedBy,
+                         ModifiedDate = sp.ModifiedDate,
+                         NumericGrade = sp.NumericGrade,
+                         SchoolId = sp.SchoolId
+                     })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.Grades.Select(sp => new GradeDTO
+                {
+                    Comments = sp.Comments,
+                    ModifiedDate = sp.ModifiedDate,
+                    SchoolId = sp.SchoolId,
+                    ModifiedBy = sp.ModifiedBy,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    GradeCodeOccurrence = sp.GradeCodeOccurrence,
+                    GradeTypeCode = sp.GradeTypeCode,
+                    NumericGrade = sp.NumericGrade,
+                    SectionId = sp.SectionId,
+                    StudentId = sp.StudentId
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Post")]
+        public async Task<IActionResult> Post([FromBody]
+                                                GradeDTO _GradeDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Grades.Where(x => x.StudentId == _GradeDTO.StudentId).FirstOrDefaultAsync();
+
+                if (itm == null)
+                {
+                    Grade g = new Grade
+                    {
+                        StudentId = _GradeDTO.StudentId,
+                        SectionId = _GradeDTO.SectionId,
+                        GradeTypeCode = _GradeDTO.GradeTypeCode,
+                        GradeCodeOccurrence = _GradeDTO.GradeCodeOccurrence,
+                        NumericGrade = _GradeDTO.NumericGrade,
+                        Comments = _GradeDTO.Comments
+                    };
+                    _context.Grades.Add(g);
+                    await _context.SaveChangesAsync();
+                    await _context.Database.CommitTransactionAsync();
+                }
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpPut]
+        [Route("Put")]
+        public async Task<IActionResult> Put([FromBody]
+                                                GradeDTO _GradeDTO)
+        {
+
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Grades.Where(x => x.StudentId == _GradeDTO.StudentId).FirstOrDefaultAsync();
+
+                itm.SectionId = _GradeDTO.SectionId;
+                itm.GradeTypeCode = _GradeDTO.GradeTypeCode;
+                itm.GradeCodeOccurrence = _GradeDTO.GradeCodeOccurrence;
+                itm.NumericGrade = _GradeDTO.NumericGrade;
+                itm.Comments = _GradeDTO.Comments;
+                
+
+                _context.Grades.Update(itm);
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("Delete/{CourseNo}")]
+        public async Task<IActionResult> Delete(int NumericGrade)
+        {
+
+            Debugger.Launch();
+
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Grades.Where(x => x.NumericGrade == NumericGrade).FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.Grades.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        public Task<IActionResult> Get(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
